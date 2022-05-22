942fa422fc4bdeb7cf81a3c7ef693492bdb6eea3
Herbert Lin <herbert.lin.7@gmail.com>
2022-05-21 22:19:17 -0500

    Save folder context menu progress

---
+++ bookmarks/src/components/dialog.tsx
@@ -0,0 +1,40 @@
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+interface RenameDialogProps {
+  open: boolean;
+  handleClose(): void; 
+}
+
+const RenameDialog = (props: RenameDialogProps) => {
+  return (
+    <Dialog open={props.open} onClose={props.handleClose}>
+      <DialogTitle>Subscribe</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          To subscribe to this website, please enter your email address here. We
+          will send updates occasionally.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleClose}>Subscribe</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export {RenameDialog};
--- bookmarks/src/components/folder-context-menu.tsx
+++ bookmarks/src/components/folder-context-menu.tsx
@@ -1,16 +1,25 @@
 import React, {useState} from 'react';
+import Divider from '@mui/material/Divider';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
-import {AnchorPosition} from './folder'
+import {AnchorPosition} from './folder';
+import { TrieNode, TrieTree } from '../data/trie';
 
 interface FolderContextMenu {
+  node: TrieNode;
   open: boolean;
   handleClose(): void;
   anchorPosition: AnchorPosition | undefined 
 }
 
 const FolderContextMenu = (props: FolderContextMenu) => {
+    const numOfChildrenPages = props.node.children?.filter(elem => elem.isPage).length;
+
+    const handleRename = async () => {
+      
+    }
+
     return (
       <Menu
         open={props.open}
@@ -18,10 +27,16 @@ const FolderContextMenu = (props: FolderContextMenu) => {
         anchorReference="anchorPosition"
         anchorPosition={props.anchorPosition}
       >
+        <MenuItem onClick={props.handleClose}>Rename</MenuItem>
+        <MenuItem onClick={props.handleClose}>Delete</MenuItem>
+        <Divider />
+        <MenuItem onClick={props.handleClose}>Cut</MenuItem>
         <MenuItem onClick={props.handleClose}>Copy</MenuItem>
-        <MenuItem onClick={props.handleClose}>Print</MenuItem>
-        <MenuItem onClick={props.handleClose}>Highlight</MenuItem>
-        <MenuItem onClick={props.handleClose}>Email</MenuItem>
+        <MenuItem onClick={props.handleClose}>Paste</MenuItem>
+        <Divider />
+        <MenuItem onClick={props.handleClose}>{`Open all (${numOfChildrenPages})`}</MenuItem>
+        <MenuItem onClick={props.handleClose}>{`Open all (${numOfChildrenPages}) in new window`}</MenuItem>
+        <MenuItem onClick={props.handleClose}>{`Open all (${numOfChildrenPages}) in incognito window`}</MenuItem>
       </Menu>
     );
   }
