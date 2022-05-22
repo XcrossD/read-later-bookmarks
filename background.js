4236462e0b03d8f58b2783177be483786bcefd54
Herbert Lin <herbert.lin.7@gmail.com>
2022-05-21 23:00:28 -0500

    Use CRA for popup

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-  console.log('icon clicked')
-  let bookmarkTreeNodes = chrome.bookmarks.getTree((bookmarkTreeNodes) => {
-    console.log(bookmarkTreeNodes)
-  })
-})
