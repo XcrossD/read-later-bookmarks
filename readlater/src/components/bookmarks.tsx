458cc087c8ebea71f5b28f779c774c837406e9b7
Herbert Lin <herbert.lin.7@gmail.com>
2022-05-29 14:53:31 -0500

    Give bookmark title eclipse effect

--- readlater/src/components/bookmarks.tsx
+++ readlater/src/components/bookmarks.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, Classes, H5 } from '@blueprintjs/core';
+import { Button, ButtonGroup, Card, Classes, H5 } from '@blueprintjs/core';
 
 interface BookmarksProps {
   readLaterFolder: chrome.bookmarks.BookmarkTreeNode|null;
@@ -53,19 +53,20 @@ const Bookmarks = (props: BookmarksProps) => {
       {props.bookmarks.map((elem, index) => {
         return (
           <Card>
-            <H5>
-                <a href={elem.url}>{elem.title}</a>
-            </H5>
             <img
               className={bookmarkMetas.length === 0 ? Classes.SKELETON : "bookmark-card-image"}
               src={bookmarkMetas.length === 0 ? 'https://picsum.photos/200' : bookmarkMetas[index].image || 'https://picsum.photos/200'}
             />
-            <p
-              className={bookmarkMetas.length === 0 ? Classes.SKELETON : ""}
-            >
-              {bookmarkMetas.length === 0 ? 'Bookmark description' : bookmarkMetas[index].description}
-            </p>
-            <Button text="Explore products" className={Classes.BUTTON} />
+            <div className="truncate">
+              <H5 className="bookmark-card-title">
+                <a href={elem.url}>{elem.title}</a>
+              </H5>
+            </div>
+            <ButtonGroup>
+              <Button icon="archive" className={Classes.BUTTON} />
+              <Button icon="social-media" className={Classes.BUTTON} />
+              <Button icon="trash" className={Classes.BUTTON} />
+            </ButtonGroup>
           </Card>
         );
       })}
