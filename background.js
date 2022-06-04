ac839fb3b506edb562d6baccfcb07720132d1753
Herbert Lin <herbert.lin.7@gmail.com>
2022-06-04 03:21:28 -0500

    Read stored settings from chrome storage

---
+++ background.js
@@ -0,0 +1,28 @@
+chrome.runtime.onInstalled.addListener((details) => {
+  const currentVersion = chrome.runtime.getManifest().version
+  const previousVersion = details.previousVersion
+  const reason = details.reason
+  
+  // console.log(`Previous Version: ${previousVersion }`)
+  // console.log(`Current Version: ${currentVersion }`)
+
+  switch (reason) {
+     case 'install':
+        console.log('New User installed the extension.')
+        chrome.storage.local.set({
+          defaultArchiveId: '1',
+          openBookmarkInNewTab: false,
+          actionOnBookmarkClicked: 'none'
+        })
+        break;
+     case 'update':
+        console.log('User has updated their extension.')
+        break;
+     case 'chrome_update':
+     case 'shared_module_update':
+     default:
+        console.log('Other install events within the browser')
+        break;
+  }
+
+})
--- options/src/App.tsx
+++ options/src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, ControlGroup, H1, H3, HTMLSelect, Label, Position } from '@blueprintjs/core';
+import { Button, ControlGroup, FormGroup, H1, H3, HTMLSelect, Label, Position, Switch } from '@blueprintjs/core';
 import { Popover2 } from '@blueprintjs/popover2';
 import React, { useEffect, useState } from 'react';
 import 'normalize.css/normalize.css';
@@ -8,8 +8,16 @@ import "@blueprintjs/popover2/lib/css/blueprint-popover2.css"
 import './App.css';
 import * as Folder from './components/folder'
 
+const DEFAULT_SETTINGS = {
+  defaultArchiveId: '1',
+  openBookmarkInNewTab: false,
+  actionOnBookmarkClicked: 'none'
+};
+
 function App() {
   const [defaultArchiveFolder, setDefaultArchiveFolder] = useState<chrome.bookmarks.BookmarkTreeNode | null>(null);
+  const [openBookmarkInNewTab, setOpenBookmarkInNewTab] = useState<boolean>(false);
+  const [actionOnBookmarkClicked, setActionOnBookmarkClicked] = useState<string>('none');
   
   useEffect(() => {
     const getAndSetFolder = async (id: string) => {
@@ -17,29 +25,64 @@ function App() {
       setDefaultArchiveFolder(result[0]);
     }
     
-    chrome.storage.local.get(['preference'], function(result) {
-      if (!result.key) {
-        chrome.storage.local.set({
-          preference: {
-            defaultArchiveId: '1'
-          }
-        }, function() {
-          getAndSetFolder('1');
+    chrome.storage.local.get(Object.keys(DEFAULT_SETTINGS), function(result) {
+      console.log("storage get result", result);
+      if (Object.keys({}).length === 0) {
+        chrome.storage.local.set(DEFAULT_SETTINGS, function() {
+          getAndSetFolder(DEFAULT_SETTINGS.defaultArchiveId);
+          setOpenBookmarkInNewTab(DEFAULT_SETTINGS.openBookmarkInNewTab);
+          setActionOnBookmarkClicked(DEFAULT_SETTINGS.actionOnBookmarkClicked);
         });
       } else {
-        getAndSetFolder(result.key.defaultArchiveId);
+        getAndSetFolder(result.defaultArchiveId);
+        setOpenBookmarkInNewTab(result.openBookmarkInNewTab);
+        setActionOnBookmarkClicked(result.actionOnBookmarkClicked);
       }
     });
 
     chrome.storage.onChanged.addListener((changes) => {
-      getAndSetFolder(changes.preference.newValue.defaultArchiveId);
+      console.log("changes", changes);
+      for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+        switch(key) {
+          case 'defaultArchiveId':
+            getAndSetFolder(newValue);
+            break;
+          case 'openBookmarkInNewTab':
+            setOpenBookmarkInNewTab(newValue);
+            break;
+          case 'actionOnBookmarkClicked':
+            setActionOnBookmarkClicked(newValue);
+            break;
+          default:
+            break;
+        }
+      }
     })
   }, []);
+
+  const handleNewTabChange = () => {
+    chrome.storage.local.set({
+      openBookmarkInNewTab: !openBookmarkInNewTab,
+    });
+  }
   
   return (
     <div className="App">
       <H1>General Settings</H1>
-      <div className="option">
+      <FormGroup
+        disabled={false}
+        inline={true}
+        label="Default archive location"
+      >
+        <Popover2
+          enforceFocus={false}
+          placement="bottom-start"
+          content={<Folder.TreeFolder defaultArchiveId={defaultArchiveFolder?.id} />}
+        >
+          <Button text={defaultArchiveFolder?.title} tabIndex={0} rightIcon="double-caret-vertical" />
+        </Popover2>
+      </FormGroup>
+      {/* <div className="option">
         <span className="option-label">Default archive location</span>
         <Popover2
           enforceFocus={false}
@@ -48,7 +91,17 @@ function App() {
         >
           <Button text={defaultArchiveFolder?.title} tabIndex={0} rightIcon="double-caret-vertical" />
         </Popover2>
-      </div>
+      </div> */}
+      <FormGroup
+        disabled={false}
+        inline={true}
+        label="Open bookmark in new tab"
+      >
+        <Switch
+          checked={openBookmarkInNewTab}
+          onChange={handleNewTabChange}
+        />
+      </FormGroup>
     </div>
   );
 }
